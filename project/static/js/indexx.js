6bad96c9465fcce896dbd09819f2c830a4177267
Abdullah00Saad <abodimahroos@gmail.com>
2022-03-03 23:32:48 +0300

    updated

---
+++ project/static/js/indexx.js
@@ -0,0 +1,21 @@
+
+         function openNav() {
+            document.getElementById("mySidenav").style.width = "220px";
+            document.getElementById("MyLogo").style.visibility = "hidden";
+            MyLogo.classList.toggle("m-fadeOut");
+
+
+
+         }
+
+         function closeNav() {
+            document.getElementById("mySidenav").style.width = "0";
+            document.getElementById("MyLogo").style.visibility = "visible";
+            MyLogo.classList.toggle("m-fadeIn");
+         }
+
+
+
+
+
+         
